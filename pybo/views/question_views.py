7e12f58f7231ea5f755fbc9cb15f44d0c7b510c0
shon5544 <sbs8239@gmail.com>
2021-07-01 22:24:32 +0900

    질문 생성 기능 완성

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
 from pybo.models import Question
+from werkzeug.utils import redirect
+from .. import db
+from ..forms import QuestionForm
+from datetime import datetime
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -15,3 +19,15 @@ def detail(question_id):
     question = Question.query.get(question_id)
     # question = Question.query.get_or_404(question_id) 아이디를 통해 question을 얻으나 없는 아이디를 쳤을때 404에러를 띄운다
     return render_template('question/question_detail.html', question=question)
+
+
+@bp.route('/create/', methods=('GET', 'POST'))
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data,
+                            content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('question/question_form.html', form=form)
