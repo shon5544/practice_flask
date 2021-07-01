2de38d274d6a61f4ab74abb928265cd11b6b7cfe
shon5544 <sbs8239@gmail.com>
2021-07-01 12:28:40 +0900

    데이터베이스에서 질문 목록 가져오기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
+@bp.route('/hello')
+def hello_pybo():
+    return '안녕하세요'
+
+
 @bp.route('/')
-def hello():
-    return '야호'
+def index():
+    return redirect(url_for('question._list'))
